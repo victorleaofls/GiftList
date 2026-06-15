61d7b1b2456d16f234217d48d676cfd9cef0e559
v-l1ma <viniciuslima957@hotmail.com>
2026-06-14 22:38:51 -0300

    adicionando variaveis de ambiente

--- ProjetoHaskell/app/Main.hs
+++ ProjetoHaskell/app/Main.hs
@@ -7,23 +7,30 @@ import Control.Monad
 import Database.PostgreSQL.Simple
 import Database.PostgreSQL.Simple.Types 
 import qualified Data.ByteString.Char8 as BS
+import System.Environment (lookupEnv)
 
 runMigration :: Connection -> FilePath -> IO ()
 runMigration conn fp = do
   sql <- readFile fp
   void $ execute_ conn (Query $ BS.pack sql)
 
+lookupEnvOr :: String -> String -> IO String
+lookupEnvOr env def = do
+    mv <- lookupEnv env
+    case mv of
+        Just v  -> pure v
+        Nothing -> pure def
+
 main :: IO ()
 main = do 
     putStrLn "Servidor rodando na porta 8080"
 
-    conn <- connectPostgreSQL
-      "host=postgres \
-      \ port=5432 \
-      \ dbname=haskads \
-      \ user=haskads_user \
-      \ password=haskads_password \
-      \ sslmode=disable"
+    host <- lookupEnvOr "PGHOST" "postgres"
+    port <- lookupEnvOr "PGPORT" "5432"
+    dbname <- lookupEnvOr "PGDATABASE" "haskads"
+    user <- lookupEnvOr "PGUSER" "haskads_user"
+    password <- lookupEnvOr "PGPASSWORD" "haskads_password"
+    conn <- connectPostgreSQL $ "host=" ++ host ++ " port=" ++ port ++ " dbname=" ++ dbname ++ " user=" ++ user ++ " password=" ++ password ++ " sslmode=disable"
 
     runMigration conn "migration.sql"
 
