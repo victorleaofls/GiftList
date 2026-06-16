5ddbc4964c2422d134d4197cbfc362a932f10991
v-l1ma <viniciuslima957@hotmail.com>
2026-06-15 21:45:30 -0300

    fix: removendo migration

--- ProjetoHaskell/app/Main.hs
+++ ProjetoHaskell/app/Main.hs
@@ -3,16 +3,9 @@ module Main (main) where
 
 import Server.Routes
 import Network.Wai.Handler.Warp
-import Control.Monad
 import Database.PostgreSQL.Simple
-import Database.PostgreSQL.Simple.Types 
 import qualified Data.ByteString.Char8 as BS
 import System.Environment (lookupEnv)
-
--- runMigration :: Connection -> FilePath -> IO ()
--- runMigration conn fp = do
---   sql <- readFile fp
---   void $ execute_ conn (Query $ BS.pack sql)
 
 main :: IO ()
 main = do 
@@ -23,7 +16,5 @@ main = do
             Just cs -> cs
             Nothing -> "host=postgres port=5432 dbname=haskads user=haskads_user password=haskads_password sslmode=disable"
     conn <- connectPostgreSQL (BS.pack connStr)
-
-    -- runMigration conn "migration.sql"
 
     run 8080 (app conn)
